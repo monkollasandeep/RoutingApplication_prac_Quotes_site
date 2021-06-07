7f078e434bc610c63e5bd27805951063d9f56893
monkollasandeep <monkollasandeep3@gmail.com>
2021-06-07 21:33:41 +0530

    Rotuing Features Added

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./Pages/AllQuotes";
+import NewQuote from "./Pages/NewQuote";
+import QuoteDetail from "./Pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes"></Redirect>
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
